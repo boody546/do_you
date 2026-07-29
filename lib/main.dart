db8ca2cfa3d61cee8b4e3f62fd6922844753f156
Abdelrahman <walaadouma56@gmail.com>
2026-07-29 21:32:36 +0300

    Fix AuthProvider naming collision in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 
 import 'firebase_options.dart';
 import 'core/theme/app_theme.dart';
